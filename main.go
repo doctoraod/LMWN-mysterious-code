a874634829ab53185fa800562ba79e78546fa701
Aod <acspdoctoraod@gmail.com>
2022-07-14 22:21:06 +0700

    Init

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+)
+
+func Reverse(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+func main() {
+	var whatIsIt string
+
+	secret := "aWFuZ25vVzpOQU06RU5JTDp0YTpzdTpuaW9K"
+	sd, _ := base64.StdEncoding.DecodeString(secret)
+
+	whatIsIt = Reverse(string(sd))
+	fmt.Print(whatIsIt)
+}
